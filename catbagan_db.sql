2a74b206c378561a07da83c27b23fd9de7622bf7
catbaganjohnpaul-qwerty <catbaganjohnpaul09@gmail.com>
2026-01-30 13:06:56 +0800

    Update catbagan_db.sql

--- catbagan_db.sql
+++ catbagan_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2026 at 03:57 PM
+-- Generation Time: Jan 30, 2026 at 05:58 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,10 +42,30 @@ CREATE TABLE `usertbl` (
 
 INSERT INTO `usertbl` (`id`, `Name`, `Email`, `Password`, `Contact`, `Gender`) VALUES
 (1, 'John Paul', 'catbagan@gmail.com', '12345678', '09087742184', 'Male'),
-(2, 'Elyza', 'flores@gmail.com', '87654321', '098745362460', 'Female'),
-(3, 'Cheiruf', 'pastedeo@gmail.com', '44444444', '09345267810', 'Male'),
-(4, 'Alex', 'delgado@gmail.com', '88888888', '09876456720', 'Female'),
-(5, 'Avery', 'sabandon@gmail.com', '22222222', '09234123980', 'Male');
+(2, 'Avery Johnson', 'sabandon@gmail.com', '87654321', '09874536280', 'Male'),
+(3, 'Princess', 'andam@gmail.com', '12124242', '09765389920', 'Female'),
+(4, 'Ace', 'gulanes@gmail.com', '54575899', '09346736210', 'Female'),
+(5, 'Gian', 'catbagan@gmail.com', '09876543', '09452819920', 'Male');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usertbl`
+--
+ALTER TABLE `usertbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usertbl`
+--
+ALTER TABLE `usertbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
